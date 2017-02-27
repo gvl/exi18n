fd37c4c2cae3c449f90f33ba7ba0bfc465bf2707
Igor Dominiak <dominiak.igor@gmail.com>
2017-02-27 13:50:26 +0100

    Add basic translation searching

--- lib/exi18n.ex
+++ lib/exi18n.ex
@@ -7,4 +7,46 @@ defmodule ExI18n do
 
   @doc "Storage type used to store translations."
   def storage, do: Application.get_env(:exi18n, :storage)
+
+  @doc """
+  Search for translation in given `locale` based on provided `key`.
+  """
+  def t(locale \\ locale(), key, values \\ %{}) do
+    translation = load_locales(locale) |> get_in(extract_keys(key))
+    cond do
+      is_bitstring(translation) -> insert_values(translation, values)
+      is_number(translation) || is_list(translation) -> translation
+      is_nil(translation) -> raise ArgumentError, "Missing translation for key: #{key}"
+      true -> raise ArgumentError, "#{key} is incomplete path to translation."
+    end
+  end
+
+  defp insert_values(text, %{}), do: text
+  defp insert_values(text, values) do
+    Enum.reduce values, text, fn({key, value}, result) ->
+      String.replace(result, "%{#{key}}", to_string(value))
+    end
+  end
+
+  defp extract_keys(key), do: String.split(key, ".")
+
+  defp load_locales(locale) do
+    init_cache()
+    case :ets.lookup(:locales, locale) do
+     [result|_] -> elem(result, 1)
+     [] ->
+       storage = ExI18n.Storage.get(storage())
+       translations = storage.load(locale)
+       :ets.insert(:locales, {locale, translations})
+       translations
+    end
+  end
+
+  defp init_cache do
+    try do
+      :ets.new(:locales, [:named_table])
+    rescue
+      _ -> nil
+    end
+  end
 end
--- test/exi18n_test.exs
+++ test/exi18n_test.exs
@@ -13,4 +13,35 @@ defmodule ExI18nTest do
   test "storage" do
     assert ExI18n.storage() == :yml
   end
+
+  test "translate returns proper translation for given path" do
+    assert ExI18n.t("en", "a") == 1
+    assert ExI18n.t("en", "b") == "test"
+    assert ExI18n.t("en", "c.d") == "test"
+    assert ExI18n.t("en", "c.e.f") == "test"
+    assert ExI18n.t("en", "g") == ["test1", "test2"]
+
+    assert ExI18n.t("en", "i", nr: 1) == "test1"
+    assert ExI18n.t("en", "i", nr: " test") == "test test"
+    assert ExI18n.t("en", "i", nr: ["1", "2"]) == "test12"
+    assert ExI18n.t("en", "i") == "test%{nr}"
+  end
+
+  test "translate raise error for non full path or missing key" do
+    assert_raise ArgumentError, "c is incomplete path to translation.", fn ->
+      ExI18n.t("en", "c")
+    end
+    assert_raise ArgumentError, "c.e is incomplete path to translation.", fn ->
+      ExI18n.t("en", "c.e")
+    end
+    assert_raise ArgumentError, "Missing translation for key: h", fn ->
+      ExI18n.t("en", "h")
+    end
+    assert_raise Protocol.UndefinedError, fn ->
+      ExI18n.t("en", "i", nr: {"1", "2"}) == "test12"
+    end
+    assert_raise Protocol.UndefinedError, fn ->
+      ExI18n.t("en", "i", nr: %{"1" => "2"}) == "test12"
+    end
+  end
 end
