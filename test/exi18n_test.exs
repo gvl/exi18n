865f5be96dfca2461226dea78eedadf75fef5e0c
Igor Dominiak <dominiak.igor@gmail.com>
2017-04-04 17:18:11 +0200

    Ensure that fallback is true at the beginning of test

--- test/exi18n_test.exs
+++ test/exi18n_test.exs
@@ -54,6 +54,7 @@ defmodule ExI18nTest do
 
   test "t/3 fallback to default locale translation if translation empty" do
     Application.put_env(:exi18n, :locales, ~w(en de))
+    Application.put_env(:exi18n, :fallback, true)
 
     assert ExI18n.t("de", "empty") == ExI18n.t("en", "empty")
 
