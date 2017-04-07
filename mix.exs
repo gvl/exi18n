9a89572415affe5a36c32ff537aa2a94cd0d1658
Igor Dominiak <dominiak.igor@gmail.com>
2017-04-07 12:07:14 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExI18n.Mixfile do
 
   def project do
     [app: :exi18n,
-     version: "0.5.2",
+     version: "0.6.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
