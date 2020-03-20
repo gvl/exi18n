5d1802ea2ba9315a447545428470ebcee7126b31
Manfred Wuits <werkzeugh@gmail.com>
2020-03-20 10:21:21 +0100

    allow fallback-translations of non-existent keys
    allow requests for keys which only exist in the fallback locale but not in the requested one

--- lib/exi18n.ex
+++ lib/exi18n.ex
@@ -85,8 +85,10 @@ defmodule ExI18n do
     if fallback(), do: get_translation(locale(), key), else: ""
   end
 
-  defp check_translation(nil, key), do: check_translation("", key)
-
+  defp check_translation(nil, key) do
+    if fallback(), do: get_translation(locale(), key), else: nil
+  end
+  
   defp check_translation(translation, _), do: translation
 
   defp check_values(translation, values) do
